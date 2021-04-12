47e112db8ce998cb4e4bc93f0f24886e9e5645d5
user-miner1 <amvnews@mail.ru>
2021-04-12 17:24:09 +0300

    Download models

--- part_zfturbo/preproc.sh
+++ part_zfturbo/preproc.sh
@@ -5,3 +5,12 @@ python3 ./preproc_data/r05_merge_boxes_v1.py
 python3 ./preproc_data/r08_prepare_yolo_dataset.py
 python3 ./preproc_data/r09_squarify_boxes.py
 python3 ./preproc_data/r12_extract_selected_radiologists.py
+mkdir ../models_inference/
+wget https://github.com/ZFTurbo/2nd-place-solution-for-VinBigData-Chest-X-ray-Abnormalities-Detection/releases/download/v1.0/yolov5x.pt -P ../models_inference/
+wget https://github.com/ZFTurbo/2nd-place-solution-for-VinBigData-Chest-X-ray-Abnormalities-Detection/releases/download/v1.0/retinanet_resnet101_500_classes_0.4986.h5 -P ../models_inference/
+wget https://github.com/ZFTurbo/2nd-place-solution-for-VinBigData-Chest-X-ray-Abnormalities-Detection/releases/download/v1.0/retinanet_resnet101_sqr.zip -P ../models_inference/
+wget https://github.com/ZFTurbo/2nd-place-solution-for-VinBigData-Chest-X-ray-Abnormalities-Detection/releases/download/v1.0/retinanet_resnet101_sqr_removed_rads.zip -P ../models_inference/
+wget https://github.com/ZFTurbo/2nd-place-solution-for-VinBigData-Chest-X-ray-Abnormalities-Detection/releases/download/v1.0/yolo_best.zip -P ../models_inference/
+unzip ../models_inference/yolo_best.zip -d  ../models_inference/yolo_best/
+unzip ../models_inference/retinanet_resnet101_sqr.zip -d  ../models_inference/retinanet_resnet101_sqr/
+unzip ../models_inference/retinanet_resnet101_sqr_removed_rads.zip -d  ../models_inference/retinanet_resnet101_sqr_removed_rads/
--- part_zfturbo/readme.md
+++ part_zfturbo/readme.md
@@ -1,5 +1,7 @@
 # How to run
 
+First you need to put [competition data](https://www.kaggle.com/c/vinbigdata-chest-xray-abnormalities-detection/data) in `../input/` folder. It will require around 190 GB of space. Preprocessing and storage of models could require plus 200 GB of space. 
+
 `pip install -r requirements.txt`
 
 ## Inference 
